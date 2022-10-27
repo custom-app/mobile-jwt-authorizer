6b5cfd7b31dbf68069e55068bce19fd0bc120289
Lev Baklanov <penachett@bk.ru>
2022-10-27 20:14:08 +0500

    ios project init

---
+++ ios/jwt_authorizer/jwt_authorizer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  jwt_authorizer
+//
+//  Created by Lev Baklanov on 27.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/jwt_authorizer/jwt_authorizer/jwt_authorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  jwt_authorizerApp.swift
+//  jwt_authorizer
+//
+//  Created by Lev Baklanov on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct jwt_authorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
